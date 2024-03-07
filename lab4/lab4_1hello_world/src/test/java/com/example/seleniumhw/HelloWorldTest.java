3f9045ef12e87e72cbd7575b903db72efa168ae1
Mateusz Kubiak <matkubiak888@gmail.com>
2024-03-07 10:45:04 +0000

    lab 4.1 b

--- lab4/lab4_1hello_world/src/test/java/com/example/seleniumhw/HelloWorldTest.java
+++ lab4/lab4_1hello_world/src/test/java/com/example/seleniumhw/HelloWorldTest.java
@@ -2,38 +2,20 @@ package com.example.seleniumhw;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.seljup.SeleniumJupiter;
 
+@ExtendWith(SeleniumJupiter.class)
 class HelloWorldTest {
-
-    WebDriver driver;
-
-    @BeforeAll
-    static void setupClass() {
-        WebDriverManager.chromedriver().setup();
-    }
-
-    @BeforeEach
-    void setup() {
-        driver = new ChromeDriver();
-    }
-
-    @AfterEach
-    void teardown() {
-        driver.quit();
-    }
 
     // the text matched is from boost c++ library site, on the right side panel below the 'welcome' section
     @Test
-    void test() {
+    void test(ChromeDriver driver) {
         driver.get("https://www.boost.org/");
 
         assertThat(driver.findElement(By.id("boost-section-menu")).getText()).contains("Introduction");
