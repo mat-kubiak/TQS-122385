7d68ca6f59c99b61bce0bcd5264627780f4a26cc
Mateusz Kubiak <matkubiak888@gmail.com>
2024-03-07 10:29:54 +0000

    lab 4.1

---
+++ lab4/lab4_1hello_world/src/test/java/com/example/seleniumhw/HelloWorldTest.java
@@ -0,0 +1,42 @@
+package com.example.seleniumhw;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+class HelloWorldTest {
+
+    WebDriver driver;
+
+    @BeforeAll
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setup() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+
+    // the text matched is from boost c++ library site, on the right side panel below the 'welcome' section
+    @Test
+    void test() {
+        driver.get("https://www.boost.org/");
+
+        assertThat(driver.findElement(By.id("boost-section-menu")).getText()).contains("Introduction");
+    }
+
+}
